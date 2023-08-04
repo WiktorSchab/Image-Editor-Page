b65efd76e0daebdf8c1214b647379a1b62f5fdb7
Wiktor Schab <wiktormateuszschab@gmail.com>
2023-08-04 10:07:47 +0200

    Added 1 color filter (red_only)

--- Image_editor_project.py
+++ Image_editor_project.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from werkzeug.utils import secure_filename
+
 import os
-from matplotlib.image import imread
-import matplotlib.pyplot as plt
 import shutil
+
 import numpy as np
 from PIL import Image, ImageFilter
+
 from img_funct import img_class
 
 app = Flask(__name__)
@@ -16,24 +17,27 @@ app.config['SECRET_KEY'] = 'P@$$w0rda'
 
 class FileForm(FlaskForm):
     cover = FileField("Send me a pic (only jpg, png need to show off validation skills)", validators=[FileRequired(),
-                                                            FileAllowed(['jpg', 'png'], "Sorry, only png and jpg")])
+                                                                                                      FileAllowed(
+                                                                                                          ['jpg',
+                                                                                                           'png'],
+                                                                                                          "Sorry, only png and jpg")])
 
 
 # starting page
 @app.route('/', methods=['POST', 'GET'])
-def index(d={'access': 0, 'filename': None},reset=0):
+def index(d={'access': 0, 'filename': None}):
     form = FileForm()
     if d['access'] == 1 or form.validate_on_submit():
         d['access'] = 1  # access to edit
-        
+
         if not d['filename'] or (form.cover.data and form.cover.data.filename != d['filename']):
-            #saving file from form
+            # saving file from form
             f = form.cover.data
             filename = secure_filename(f.filename)
             path = os.path.join(app.root_path, 'static', 'download', 'modified', filename)
             f.save(path)
 
-            #resizing file for one format of display and fast operating on file
+            # resizing file for one format of display and fast operating on file
             img = np.array(Image.open(path).resize((1024, 1024)))
             img_class.save_img(img, path)
 
@@ -42,20 +46,20 @@ def index(d={'access': 0, 'filename': None},reset=0):
 
             # saving values to dict, so they won't be refreshed
             d['filename'] = filename
-
         filename = d['filename']
-        print(filename,'tututututu')
+
         return render_template('work_page.html', file_name=filename)
     return render_template('index.html', form=form)
 
-#Changing image
+
+# Changing image
 @app.route('/deleting_image')
 def img_change():
     form = FileForm()
     return render_template('index.html', form=form)
 
 
-#Reset img
+# Reset img
 @app.route('/reset_image/<file_name>')
 def img_reset(file_name):
     path = os.path.join(app.root_path, 'static', 'download', 'original', file_name)
@@ -63,7 +67,7 @@ def img_reset(file_name):
     return redirect(url_for('index'))
 
 
-#Black & white filter
+# Black & white filter
 @app.route('/bw_filter/<file_name>')
 def bw_filter(file_name):
     path = os.path.join(app.root_path, 'static', 'download', 'original', file_name)
@@ -73,7 +77,7 @@ def bw_filter(file_name):
     return redirect(url_for('index'))
 
 
-#Real black & white filter
+# Real black & white filter
 @app.route('/rbw_filter/<file_name>')
 def rbw_filter(file_name):
     path = os.path.join(app.root_path, 'static', 'download', 'original', file_name)
@@ -83,7 +87,7 @@ def rbw_filter(file_name):
     return redirect(url_for('index'))
 
 
-#Contur filter
+# Contur filter
 @app.route('/con_filter/<file_name>')
 def con_filter(file_name):
     path = os.path.join(app.root_path, 'static', 'download', 'original', file_name)
@@ -93,7 +97,7 @@ def con_filter(file_name):
     return redirect(url_for('index'))
 
 
-#Blur filter
+# Blur filter
 @app.route('/blur_filter/<file_name>')
 def blur_filter(file_name):
     path = os.path.join(app.root_path, 'static', 'download', 'original', file_name)
@@ -103,13 +107,26 @@ def blur_filter(file_name):
     return redirect(url_for('index'))
 
 
-#Emboss filter
+# Emboss filter
 @app.route('/emb_filter/<file_name>')
 def emb_filter(file_name):
     path = os.path.join(app.root_path, 'static', 'download', 'original', file_name)
 
     img = np.array(Image.open(path).filter(ImageFilter.EMBOSS))
     img_class.save_img(img, path.replace('original', 'modified'))
+    return redirect(url_for('index'))
+
+
+# color filters
+@app.route('/only_red/<file_name>')
+def only_red(file_name):
+    path = os.path.join(app.root_path, 'static', 'download', 'original', file_name)
+
+    lower = (119, 100, 100)
+    upper = (129, 255, 255)
+
+    result = img_class.display_color(path, lower, upper)
+    img_class.save_img(result, path.replace('original', 'modified'))
     return redirect(url_for('index'))
 
 
