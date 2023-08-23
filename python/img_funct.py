aec3cdae7948e5e479b25fa0c83587b387fbff36
WiktorSchab <wiktormateuszschab@gmail.com>
2023-08-23 10:30:45 +0200

    Updated the code of img_funct.py
    Updated img_funct.py to meet better standards

--- python/img_funct.py
+++ python/img_funct.py
@@ -1,7 +1,8 @@
-from PIL import Image
-
+# Modul needed to accurate measuring time
 from time import perf_counter
 
+# Modul needed to convert array into img
+from PIL import Image
 
 def save_img(img, path):
     """Function to save img in delivered path
@@ -30,7 +31,9 @@ def performance():
 
 
 # Function to give id of nick
-def get_nick_id(nick, User):
-    # getting id of user
-    user = User.query.filter(User.nick == nick).first()
+def get_nick_id(nick, table):
+    """Function to get user id of nick in given table
+    Function returns id of nick in table"""
+
+    user = table.query.filter(table.nick == nick).first()
     return user.id
